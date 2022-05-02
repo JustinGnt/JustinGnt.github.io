6660e3076d27c642745ebd697446fc81f4ba3beb
JustinGall <99751930+JustinGall@users.noreply.github.com>
2022-05-02 18:36:41 -0300

    Alpha 1

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 let clicks = 0;
 let noobObj = document.querySelector('.clicks');
 
+let pokeDisplay = document.querySelector('.buy-poke-stick');
+let squeakDisplay = document.querySelector('.buy-squeak');
 
+let pokeLvl = document.querySelector('.poke-level');
+let squeakLvl = document.querySelector('.squeak-level');
+
+let pokeStrength = 1;
+
+let robuxCost = 1500
+let squeakCost = 500;
+let pokeCost = 50;
+
+let squeaker = 2;
+let robux = 6;
+
+let x = 0;
+
+let pokeLevel = 1;
+let squeakLevel = 1;
+let robuxLevel = 1;
+
+var seconds = 0;
 
 // Onclick function
 function noobClick() {
@@ -9,42 +30,72 @@ function noobClick() {
     var ben = new Audio('audio/ben.mp3')
 
     // Random colour change interval
-    let random16 = Math.floor(Math.random() * 16);
+    let random16 = Math.floor(Math.random() * 5);
 
-    clicks += 1;
-    noobObj.textContent = `Noob has been clicked: ${clicks} times! `;
-    console.log(clicks)
+    // Random ben interval
+    let randomBen = Math.floor(Math.random() * 15 );
 
+    // Random cursed interval
+    let cursedRand = Math.floor(Math.random() * 25 );
+
+    clicks += pokeStrength;
+    noobObj.textContent = "Noob has been clicked: " + Math.ceil(clicks) + " times!";
 
     // Plays "behn" sound
-    if (clicks % 10 === 0) {
-
+    if (randomBen === 1) {
         ben.play();
-
     }
 
     // Adds cursed image
-    if (clicks % 100 === 0) {
-
+    if (cursedRand === 1) {
         document.body.style.backgroundImage = "url('imgs/cursed.jpg')"
-
-
     }
 
     // Randomly changes the background colour
-    if (clicks % random16 === 0) {
-
+    if (random16 === 1) {
         var x = Math.floor(Math.random() * 256);
         var y = Math.floor(Math.random() * 256);
         var z = Math.floor(Math.random() * 256);    
         var bgColour = "rgb(" + x + "," + y + "," + z + ")";
-        console.log(bgColour)
 
         document.body.style.backgroundColor = bgColour;
-        document.body.style.backgroundImage = ""
+        document.body.style.backgroundImage = "";
+    }
+}
+
+function upgradeFunc(name) {
+
+    if (name === "poke" && clicks >= pokeCost) {
+        pokeLevel += 1;
+        clicks -= pokeCost;
+        noobObj.textContent = `Noob has been clicked: ${Math.ceil(clicks)} times! `;
+        pokeCost = pokeCost * 1.45;
+        pokeStrength = pokeStrength * 1.5;
+        
+        pokeLvl.textContent = "Poke Stick -- Lvl: " + pokeLevel;
+        pokeDisplay.textContent = "Upgrade \r\n (" + Math.ceil(pokeCost) + " clicks)";
+    }
+
+    if (name === "squeak" && clicks >= squeakCost ) {
+        squeakLevel += 1;
+        clicks -= squeakCost;
+        noobObj.textContent = `Noob has been clicked: ${Math.ceil(clicks)} times! `;
+        squeakCost = squeakCost * 1.33;
+        squeaker = squeaker * 1.5;
+        window.setInterval(function(){
+
+            clicks = clicks += squeaker;
+            noobObj.textContent = `Noob has been clicked: ${Math.ceil(clicks)} times! `;
+
+        }, 100);
+
+        squeakLvl.textContent = "Squeaker -- Lvl: " + squeakLevel;
+        squeakDisplay.textContent = "Upgrade \r\n (" + Math.ceil(squeakCost) + " clicks)";
     }
 
 
+
+var cancel = setInterval(incrementSeconds, 1000);
 }
 
 // comment
